6673090f22a2db8fc1536195b4227bf60118ca50
brice-morin <brice.morin1@gmail.com>
2014-09-10 14:11:37 +0200

    update

--- WeatherStationArduino/src/main/java/org/thingml/tobegenerated/REST_RemoteControl_Server.java
+++ WeatherStationArduino/src/main/java/org/thingml/tobegenerated/REST_RemoteControl_Server.java
@@ -8,12 +8,6 @@ import javax.ws.rs.core.MediaType;
 @Path("/RemoteControl")
 public class REST_RemoteControl_Server {
 
-    WeatherStation ws;
-
-    public REST_RemoteControl_Server(WeatherStation ws) {
-        this.ws = ws;
-    }
-
 /*    @GET @Path("/display")
     @Produces(MediaType.TEXT_PLAIN)
     public String display() {
@@ -25,7 +19,7 @@ public class REST_RemoteControl_Server {
     @Produces(MediaType.TEXT_PLAIN)
    // @Consumes(MediaType.TEXT_PLAIN)
     public String changeDisplay() {
-        ws.changeDisplay_via_gui();
+        WeatherStation.getInstance().changeDisplay_via_gui();
         System.out.println("Got changeDisplay");
         return "OK";
     }
