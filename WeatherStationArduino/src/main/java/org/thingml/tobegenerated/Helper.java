622e864b8d8fad2a35433d96465562b65c810b74
brice-morin <brice.morin1@gmail.com>
2014-09-11 10:17:19 +0200

    added freeboard dashboard for the weather station

---
+++ WeatherStationArduino/src/main/java/org/thingml/tobegenerated/Helper.java
@@ -0,0 +1,11 @@
+package org.thingml.tobegenerated;
+
+/**
+ * Created by bmori on 10.09.2014.
+ */
+public class Helper {
+
+    static float temperature;
+    static float light;
+
+}
--- WeatherStationArduino/src/main/java/org/thingml/tobegenerated/REST_M2M_Mock.java
+++ WeatherStationArduino/src/main/java/org/thingml/tobegenerated/REST_M2M_Mock.java
@@ -2,15 +2,45 @@ package org.thingml.tobegenerated;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/M2MMock")
 public class REST_M2M_Mock {
+
+    @GET @Path("/temperature")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response GETTemperature() {
+        /*System.out.println("DEBUG GET temperature : " + Helper.temperature);
+        return "{\"temperature\":" + Helper.temperature + "}";*/
+
+        //CORS support (even needed for localhost)
+        return Response.ok() //200
+                .entity("{\"temperature\":" + Helper.temperature + "}")
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .allow("OPTIONS").build();
+    }
+
+    @GET @Path("/light")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response GETLight() {
+        /*System.out.println("DEBUG GET light : " + Helper.light);
+        return "{\"light\":" + Helper.light + "}";*/
+
+        //CORS support (even needed for localhost)
+        return Response.ok() //200
+                .entity("{\"light\":" + Helper.light + "}")
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .allow("OPTIONS").build();
+    }
 
     @PUT @Path("/temperature")
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.TEXT_PLAIN)
     public String temperature(String message) {
-        System.out.println("Got temperature : " + Float.parseFloat(message));
+        Helper.temperature = Float.parseFloat(message);
+        System.out.println("Got temperature : " + Helper.temperature);
         return "OK temperature:"+message;
     }
 
@@ -18,7 +48,8 @@ public class REST_M2M_Mock {
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.TEXT_PLAIN)
     public String light(String message) {
-        System.out.println("Got light : " + Float.parseFloat(message));
+        Helper.light = Float.parseFloat(message);
+        System.out.println("Got light : " + Helper.light);
         return "OK light:"+message;
     }
 
