741f9642e04d8907426da90077d1aedaf5bf8249
Franck Fleurey <franck@fleurey.com>
2014-09-15 12:36:18 +0200

    Security fix

--- WeatherStation/src/main/java/org/thingml/samples/weatherstation/REST_RemoteControl_Server.java
+++ WeatherStation/src/main/java/org/thingml/samples/weatherstation/REST_RemoteControl_Server.java
@@ -2,6 +2,7 @@ package org.thingml.samples.weatherstation;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/RemoteControl")
 public class REST_RemoteControl_Server {
@@ -9,17 +10,25 @@ public class REST_RemoteControl_Server {
 
     @GET @Path("/display")
     @Produces(MediaType.TEXT_PLAIN)
-    public String display() {
-        return WeatherStationSimulator.getInstance().getDisplayString();
+    public Response display() {
+         return Response.ok() //200
+                .entity(WeatherStationSimulator.getInstance().getDisplayString())
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .allow("OPTIONS").build();
     }
 
     @PUT @Path("/changeDisplay")
     @Produces(MediaType.TEXT_PLAIN)
    // @Consumes(MediaType.TEXT_PLAIN)
-    public String changeDisplay() {
+    public Response changeDisplay() {
         WeatherStationSimulator.getInstance().RemoteControl_changeDisplay();
         System.out.println("Got changeDisplay");
-        return "OK";
+        return Response.ok() //200
+                .entity("OK")
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .allow("OPTIONS").build();
     }
 
 }
